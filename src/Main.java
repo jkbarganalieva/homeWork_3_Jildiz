57ae688f75d0ee1b0ee5837f4dcc57e649e5db32
barganalieva <jkbarganalieva@gmail.com>
2022-10-08 21:26:29 +0300

    добавила счетчик, строка 12

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,22 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
        double[] digits ={5.3, -3, 15, -6.1, -9, -13, 14, 8.9, -11, 16, 2, 3, 1.5, 14, 12};
-               double znach=0;
+       int k=0;
+       double znach=0;
         for (double num:digits) {
             if (num<0){
                 continue;
             }
+            k=k+1;
             znach=znach+num;
         }
+        System.out.println(k);
         System.out.println(Arrays.toString(digits));
         System.out.println(znach);
-        System.out.println("sr="+znach/10);
+        System.out.println("sr="+znach/k);
 
 
-        for (int i = 0; i < digits.length-1; i++) {
+       /* for (int i = 0; i < digits.length-1; i++) {
             for (int j = 0; j < digits.length ; j++) {
                 if (digits[i]>digits[j]){
                         double b=digits[i];
@@ -29,7 +32,7 @@ public class Main {
                 //System.out.println(j+1, digits[j]);
             }
 
-        }
+        }*/
     }
 }
 
