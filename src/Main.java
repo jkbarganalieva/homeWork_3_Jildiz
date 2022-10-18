880154bb6553a0f8629fbc57d64fb7bc11c4da78
barganalieva <jkbarganalieva@gmail.com>
2022-10-18 10:41:50 +0300

    добавила счетчик, строка 12 и тип буулин

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,35 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-       double[] digits ={5.3, -3, 15, -6.1, -9, -13, 14, 8.9, -11, 16, 2, 3, 1.5, 14, 12};
-       int k=0;
-       double znach=0;
-        for (double num:digits) {
-            if (num<0){
-                continue;
-            }
-            k=k+1;
-            znach=znach+num;
-        }
+        double[] digits = {5.3, -3, 15, -6.1, -9, -13, 14, 8.9, -11, 16, 2, 3, 1.5, 14, 12};
+        int k = 0;
+        double znach = 0;
+        boolean bool=false;
+        for (double num : digits) {
+            if (num < 0) {
+                bool=true;
+            }else if(bool){
+            k = k + 1;
+            znach = znach + num;
+        }}
         System.out.println(k);
         System.out.println(Arrays.toString(digits));
         System.out.println(znach);
-        System.out.println("sr="+znach/k);
+        System.out.println("sr=" + znach / k);
+       System.out.println("==="+min(digits));
 
-
-       /* for (int i = 0; i < digits.length-1; i++) {
-            for (int j = 0; j < digits.length ; j++) {
-                if (digits[i]>digits[j]){
-                        double b=digits[i];
-                        digits[i]=digits[j];
-                                digits[j]=b;
-                }
-
-            }
-            //for (double j = 0; j < digits.length; j++) {
-                System.out.println(Arrays.toString(digits));
-                //System.out.println(j+1, digits[j]);
+           }
+    public static double min(double[] digits ){
+        double minInbox=0;
+        double minValue=digits[0];
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i]<minValue){
+                minValue=digits[i];
+                minInbox=i;
             }
 
-        }*/
+        }
+        return minInbox+minValue;
     }
 }
 
